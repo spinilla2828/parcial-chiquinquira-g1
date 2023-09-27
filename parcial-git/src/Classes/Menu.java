1f523f5cb65bf84337706a309c0a1d8e48763662
DiegoMendez01 <diegomendez01soft@gmail.com>
2023-09-26 20:18:30 -0500

    Agregar validacion de entrada de letras en inputs numericos

--- parcial-git/src/Classes/Menu.java
+++ parcial-git/src/Classes/Menu.java
@@ -21,20 +21,36 @@ public class Menu {
             System.out.println("4. Mostrar todos los estudiantes.");
             System.out.println("5. Salir del programa.");
             System.out.print("Seleccione una opcion: ");
+            
+            while (!scanner.hasNextInt()) {
+                System.out.println("\nOpcion no valida. Intente de nuevo.");
+                System.out.print("Seleccione una opcion: ");
+                scanner.next();
+            }
             opcion = scanner.nextInt();
-            scanner.nextLine();
+            scanner.nextLine(); // Limpiar el buffer
             
             switch (opcion) {
                 case 1:
                     // Caso 1 agregar estudiante aqui
-                	System.out.print("Ingrese el nombre del estudiante: ");
+                	System.out.print("\nIngrese el nombre del estudiante: ");
                     String nombre = scanner.nextLine();
                     System.out.print("Ingrese la edad del estudiante: ");
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("Edad no valida. Intente de nuevo.");
+                        System.out.print("Ingrese la edad del estudiante: ");
+                        scanner.next();
+                    }
                     int edad = scanner.nextInt();
                     scanner.nextLine(); // Limpiar el buffer
                     List<String> asignaturas = new ArrayList<>();
 
                     System.out.print("Ingrese la cantidad de asignaturas en las que esta inscrito: ");
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("Numero de asignaturas no valido. Intente de nuevo.");
+                        System.out.print("Ingrese la cantidad de asignaturas en las que esta inscrito: ");
+                        scanner.next();
+                    }
                     int numAsignaturas = scanner.nextInt();
                     scanner.nextLine(); // Limpiar el buffer
 
@@ -49,14 +65,14 @@ public class Menu {
                     break;
                 case 2:
                     // Caso 2 buscar estudiante aqui
-                	System.out.println("Ingrese el nombre del estudiante: ");
+                	System.out.println("\nIngrese el nombre del estudiante: ");
                 	String nombreBuscar = scanner.nextLine();
                 	
                 	buscarEstudiante.BuscarEstudiantePorNombre(listaEstudiantes, nombreBuscar);
                     break;
                 case 3:
                     // Caso 3 buscar estudiantes por asignatura aqui
-                	System.out.println("Ingrese el nombre de la asignatura: ");
+                	System.out.println("\nIngrese el nombre de la asignatura: ");
                 	String asignatura = scanner.nextLine();
                 	
                 	buscarEstudianteAsignatura.buscarEstudiantePorAsignatura(listaEstudiantes, asignatura);
@@ -70,7 +86,7 @@ public class Menu {
                     scanner.close();
                     System.exit(0);
                 default:
-                    System.out.println("Opción no válida. Intente de nuevo.\n");
+                    System.out.println("Opcion no valida. Intente de nuevo.\n");
             }
         }while(opcion != 5);
     }
