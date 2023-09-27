ba0f6319c0bb3634497852b971208187401dbfde
DiegoMendez01 <diegomendez01soft@gmail.com>
2023-09-26 20:36:59 -0500

    Agregar comentarios a las clases BuscarEstudiante, AgregarEstudiante y Estudiantes

--- parcial-git/src/Classes/BuscarEstudiante.java
+++ parcial-git/src/Classes/BuscarEstudiante.java
@@ -3,8 +3,15 @@ package Classes;
 import java.util.List;
 
 public class BuscarEstudiante {
+	/**
+     * Busca un estudiante por su nombre en la lista de estudiantes y muestra sus detalles si se encuentra.
+     * 
+     * @param listaEstudiantes La lista de estudiantes en la que se realizara la busqueda.
+     * @param nombreBuscar     El nombre del estudiante que se busca.
+     */
 	public void BuscarEstudiantePorNombre(List<Estudiantes> listaEstudiantes, String nombreBuscar) {
         boolean encontrado = false;
+       //Recorre la lista de estudiantes para buscar el estudiante por nombre
         for(Estudiantes estudiante : listaEstudiantes){
         	if (estudiante.getNombre().equalsIgnoreCase(nombreBuscar)) {
         		System.out.println("Nombre: "+estudiante.getNombre());
@@ -14,6 +21,7 @@ public class BuscarEstudiante {
         		break;
         	}
         }
+        //Si no se encuentra el estudiante, muestra un mensaje
         if(!encontrado) {
         	System.out.println("No se encontro el estudiante");
         }
